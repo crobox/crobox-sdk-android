d0c9f98dbcfc8307489eda4b2ba08012331e9d43
idrisyildiz <idrisyildiz7@gmail.com>
2024-05-29 15:02:42 +0300

    Crobox API and APClient

--- CroboxTestApp/croboxSDK/src/main/java/com/crobox/sdk/Data/API/CroboxAPI.kt
+++ CroboxTestApp/croboxSDK/src/main/java/com/crobox/sdk/Data/API/CroboxAPI.kt
@@ -1,4 +1,15 @@
 package com.crobox.sdk.Data.API
 
-class CroboxAPI {
+import com.crobox.sdk.Domian.PromotionsResponse
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.http.*
+
+interface CroboxAPI {
+
+    @POST("/promotions")
+    fun promotions(@Body requestBody: RequestBody): Call<PromotionsResponse?>?
+
+    @POST("/promotions")
+    fun socket(@Body requestBody: RequestBody): Call<PromotionsResponse?>?
 }
--- CroboxTestApp/croboxSDK/src/main/java/com/crobox/sdk/Data/API/CroboxAPIClient.kt
+++ CroboxTestApp/croboxSDK/src/main/java/com/crobox/sdk/Data/API/CroboxAPIClient.kt
@@ -1,4 +1,38 @@
 package com.crobox.sdk.Data.API
 
-class CroboxAPIClient {
+import com.crobox.sdk.Domian.Constant
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+internal object CroboxAPIClient {
+    val clientWithOutToken: Retrofit
+        get() {
+            val headerLogging = HttpLoggingInterceptor()
+            val bodyLogging = HttpLoggingInterceptor()
+
+            headerLogging.setLevel(HttpLoggingInterceptor.Level.HEADERS)
+            bodyLogging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+            val client: OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(bodyLogging)
+                .readTimeout(60 * 5, TimeUnit.SECONDS)
+                .connectTimeout(60 * 5, TimeUnit.SECONDS)
+                .addInterceptor(
+                    Interceptor { chain: Interceptor.Chain ->
+                        var request: Request.Builder = chain.request().newBuilder()
+                        request = request.method(chain.request().method, chain.request().body)
+                        chain.proceed(request.build())
+                    }).build()
+
+            return Retrofit.Builder()
+                .client(client)
+                .baseUrl(Constant.API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
 }
