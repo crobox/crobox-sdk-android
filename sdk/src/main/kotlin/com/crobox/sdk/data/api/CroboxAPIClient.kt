7183147bd4b69dda53174ebf2ca221b7ce8d24dc
Tayfun Oztemel <tayfun@crobox.com>
2024-07-12 14:54:51 +0200

    clean up

--- sdk/src/main/kotlin/com/crobox/sdk/data/api/CroboxAPIClient.kt
+++ sdk/src/main/kotlin/com/crobox/sdk/data/api/CroboxAPIClient.kt
@@ -3,7 +3,6 @@ package com.crobox.sdk.data.api
 import com.google.gson.GsonBuilder
 import com.google.gson.Strictness
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.UUID
@@ -28,7 +27,6 @@ internal object CroboxAPIClient {
 
     private fun client(): OkHttpClient {
         val client: OkHttpClient = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .readTimeout(60 * 5, TimeUnit.SECONDS)
             .connectTimeout(60 * 5, TimeUnit.SECONDS).build()
 
